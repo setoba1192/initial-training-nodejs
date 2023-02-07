4956def7acfc2ad62e23605efb39234657e13164
Joan <setoba1192@hotmail.com>
2023-02-06 22:45:41 -0500

    event cycles in Node

---
+++ 01-hello-world/app2.js
@@ -0,0 +1,5 @@
+const greet = (name) => {
+    return `Greetings ${name}`;
+}
+
+console.log(greet('Sebastian'))
---
+++ 01-hello-world/app3.js
@@ -0,0 +1,14 @@
+console.log('Start program')
+
+setTimeout( () =>{
+    console.log('First TimeOut')
+}, 3000)
+
+setTimeout( () =>{
+    console.log('Second TimeOut')
+}, 0)
+
+setTimeout( () =>{
+    console.log('Third TimeOut')
+}, 0)
+console.log('End program')
