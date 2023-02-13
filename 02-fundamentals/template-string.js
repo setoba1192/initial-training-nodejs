e082e8ac70a0bc47681e6aa76640fe526f6d7f81
Joan <setoba1192@hotmail.com>
2023-02-12 22:14:07 -0500

    template string

---
+++ 02-fundamentals/template-string.js
@@ -0,0 +1,19 @@
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre + ' ' + real;
+const template = `${nombre} ${real}`
+const template2 = `${1 + 1} ${real}`
+
+console.log(normal);
+console.log(template);
+console.log(template2);
+
+console.log(template == normal)
+
+const html = `
+<h1>Hola</h1>
+<p>Mundo</p>
+`;
+
+console.log(html)
