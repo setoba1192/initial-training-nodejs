2c1cf21f8f34e5e7e4eae2f5609fd6a77d5d3c1c
Joan <setoba1192@hotmail.com>
2023-03-04 22:18:41 -0500

    construir menú interactivo - inquirer

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,18 +1,15 @@
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer-menu')
 
 require('colors');
 
-console.clear();
+//console.clear();
 
 const main = async () => {
 
     let opt = '';
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({ opt });
-
-        if (opt !== '0')
-            await pausa();
 
     } while (opt !== '0')
 
---
+++ 04-tareas-hacer/helpers/inquirer-menu.js
@@ -0,0 +1,28 @@
+require('colors');
+const inquirer = require('inquirer');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: ['opt1', 'op2', 'opt3']
+    }
+]
+
+const inquirerMenu = async () => {
+    
+    //console.clear();
+    console.log('=========================='.green);
+    console.log('   Seleccione una opción'.green);
+    console.log('==========================\n'.green);
+
+    const opt = await inquirer.prompt(preguntas);
+
+    return opt;
+}
+
+
+module.exports = {
+    inquirerMenu
+}
