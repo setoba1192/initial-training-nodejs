28f89a26df90ea5a3b894771b2a71a65693f7a80
Joan <setoba1192@hotmail.com>
2023-03-12 23:40:23 -0500

    Mapbox search API y Token de acceso

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -12,7 +12,7 @@ class Busquedas {
 
         try {
             //peticion http
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/ottawa.json?proximity=ip&language=es&access_token=pk.eyJ1Ijoic2V0b2JhMTE5MiIsImEiOiJjbGY2Ym1wajQwNm1zM3NwbmwzNWo1ejNlIn0.Su0dkU4dNqBaPjhe9hGIXA')
             console.log(resp.data);
 
         } catch (error) {
