6e869d2f37c77e7c457e78c0cabd2b5935a60fba
Joan <setoba1192@hotmail.com>
2023-03-16 22:32:51 -0500

    servir contenido estático

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -4,16 +4,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+// Servir contenido stático
+app.use(express.static("public"));
 
 app.get("/hola-mundo", (req, res) => {
   res.send("Ahora si, hola mundo!");
 });
 
 app.get("/*", (req, res) => {
-  res.send("404 | page not found");
+  res.sendFile(__dirname+"/public/404.html");
 });
 app.listen(port, () => {
   console.log(`Running app on port ${port}`);
