8d250902833e7192d40e184c6ab44e8362606d24
Joan <setoba1192@hotmail.com>
2023-03-17 22:56:05 -0500

    handlebars

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -4,15 +4,21 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "hbs");
+
 // Servir contenido stático
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home")
+});
+
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname+"/public/elements.html");
+  res.sendFile(__dirname + "/public/elements.html");
 });
 
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname+"/public/generic.html");
+  res.sendFile(__dirname + "/public/generic.html");
 });
 
 app.get("/hola-mundo", (req, res) => {
@@ -20,7 +26,7 @@ app.get("/hola-mundo", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(__dirname+"/public/404.html");
+  res.sendFile(__dirname + "/public/404.html");
 });
 app.listen(port, () => {
   console.log(`Running app on port ${port}`);
